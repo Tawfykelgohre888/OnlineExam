a0ba2f3c5a60954629635071f83cbc7ad4c8d46c
Tawfykelgohre888 <tawfykelgohre8@gmail.com>
2025-08-11 14:48:13 +0300

    fixBugDashboard

--- src/app/core/pages/dashboard/dashboard.component.ts
+++ src/app/core/pages/dashboard/dashboard.component.ts
@@ -1,32 +1,44 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { initFlowbite } from 'flowbite';
 import { SubjectService } from '../../services/subject.service';
 import { Subject } from '../../interface/get-subject';
+
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent implements OnInit {
-  _sub = inject(SubjectService)
-  subjects:Subject[] = [];
+  _sub = inject(SubjectService);
+  cdr = inject(ChangeDetectorRef);
+  subjects: Subject[] = [];
+  limit: number = 9;
 
-    ngOnInit(): void {
+  ngOnInit(): void {
     initFlowbite();
-    this.displaySubject()
+    this.displaySubject();
   }
 
-
-
-  displaySubject():void{
+  displaySubject(): void {
     this._sub.getSubject().subscribe({
-      next:(res)=>{
+      next: (res) => {
         console.log(res);
-        this.subjects = res.subjects
-      },error:(err)=>{
+        this.subjects = res.subjects;
+        console.log('Subjects length:', this.subjects.length);
+      },
+      error: (err) => {
         console.log(err);
-      }
-    })
+      },
+    });
+  }
+
+  viewAll() {
+    this.limit = this.subjects.length;
+    console.log('limit updated:', this.limit);
+    this.cdr.detectChanges();
   }
 }
