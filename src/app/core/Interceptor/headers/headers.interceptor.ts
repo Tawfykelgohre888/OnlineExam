17fc50470bf85dc74ee6216e12c8a505c1e0614a
Tawfykelgohre888 <tawfykelgohre8@gmail.com>
2025-08-16 00:16:16 +0300

    handel

--- src/app/core/Interceptor/headers/headers.interceptor.ts
+++ src/app/core/Interceptor/headers/headers.interceptor.ts
@@ -1,7 +1,13 @@
+import { isPlatformBrowser } from '@angular/common';
 import { HttpInterceptorFn } from '@angular/common/http';
+import { inject, PLATFORM_ID } from '@angular/core';
 
 export const headersInterceptor: HttpInterceptorFn = (req, next) => {
-  if (typeof window !== 'undefined' && localStorage.getItem('userToken')) {
+
+  const pLATFORM_ID = inject(PLATFORM_ID)
+
+
+  if (isPlatformBrowser(pLATFORM_ID)) {
     req = req.clone({
       setHeaders: {
         token: localStorage.getItem('userToken')!
